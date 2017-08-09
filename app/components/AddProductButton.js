bbf85b6cff273315122cb521c38af32927397c0e
Daniel Bruce <danieljbruce@gmail.com>
2017-08-08 21:05:44 -0400

    Made function less anonymous to setup for componen

--- app/components/AddProductButton.js
+++ app/components/AddProductButton.js
@@ -4,7 +4,7 @@
 
 import React from 'react';
 
-const AddProductButton = () =>
-    <button> Add item </button>;
-
+const AddProductButton = function render() {
+    return <button> Add item </button>;
+};
 export default AddProductButton;

f4b4b35d7caa90814a9aa3d9db25e7638e030c9d
Daniel Bruce <danieljbruce@gmail.com>
2017-08-23 00:22:09 -0400

    changed reducers to include products

--- app/reducers/index.js
+++ app/reducers/index.js
@@ -1,9 +1,8 @@
 import { routerReducer as routing } from 'react-router-redux';
-import { combineReducers } from 'redux';
+// import { combineReducers } from 'redux';
 import * as types from '../actions/types';
 
-const filter = (state = '', action) => {
-    console.log(action);
+const filterReducer = (state = '', action) => {
     switch (action.type) {
         case types.FILTER:
             return action.filter;
@@ -12,21 +11,46 @@ const filter = (state = '', action) => {
     }
 };
 
-const nextProduct = (state = '', action) => {
-    console.log(action);
+// const nextProduct = (state = '', action) => {
+//     switch (action.type) {
+//         case 'CHANGE_NEXT_PRODUCT':
+//             return action.nextProduct;
+//         default:
+//             return state;
+//     }
+// };
+//
+// const products = (state = [], action) => {
+//     switch (action.type) {
+//         case 'ADD_NEXT_PRODUCT':
+//             return state.push[action.nextProduct];
+//         default:
+//             console.log('bear');
+//             return state;
+//     }
+// };
+
+const crossSliceReducer = (state = {nextProduct: '', products: []}, action) => {
     switch (action.type) {
         case 'CHANGE_NEXT_PRODUCT':
-            return action.nextProduct;
+            return {'nextProduct': action.nextProduct, 'products': state.products};
+        case 'ADD_NEXT_PRODUCT':
+            return {'nextProduct': state.nextProduct, 'products': state.products.push[state.nextProduct]};
         default:
-            console.log('bear');
+            if (typeof(state.nextProduct) === 'undefined') {
+                state.nextProduct = '';
+            }
+            if (typeof(state.products) === 'undefined') {
+                state.products = [];
+            }
             return state;
     }
 };
 
-const rootReducer = combineReducers({
-    filter,
-    nextProduct,
-    routing
-});
+const rootReducer = (state = {}, action) => {
+    const filter = filterReducer(state.filter, action);
+    const crossSlice = crossSliceReducer({nextProduct: state.nextProduct, products: state.products}, action);
+    return {filter: filter, nextProduct: crossSlice.nextProduct, products: crossSlice.products, routing: routing(state.routing)};
+};
 
 export default rootReducer;
